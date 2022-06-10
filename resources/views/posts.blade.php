f5eb2c0c865c0b9eb396eaa308c08fc862c5726a
Alexander Hoinville <ahoin001@gmail.com>
2022-06-10 12:52:01 -0400

    Styled featured blog card

--- resources/views/posts.blade.php
+++ resources/views/posts.blade.php
@@ -1,31 +1,12 @@
 <x-layout>
 
-    {{-- <x-slot name="content"> --}}
-    {{-- @foreach ($posts as $post) --}}
-    {{-- <article>
-                <h1> 
-                    <a href="/posts/{{$post->id}}">
-                        {{$post->title }}
-                    </a> 
-                </h1> 
-                <p>By: <a href="/authors/{{$post->author->username}}">{{$post->author->name}}</a></p> --}}
-    {{-- * ->category returns category asscoiated with post, then we can access columns of category --}}
-    {{-- <a href="/categories/{{$post->category->slug}}">{{$post->category->name}}</a>
-                <div> {!! $post->excerpt !!}</div> --}}
-    {{-- </article>
-    
-        @endforeach --}}
-
-
-    {{-- </x-slot> --}}
-
     @include('_posts-header')
 
     <main class="max-w-6xl mx-auto mt-6 lg:mt-20 space-y-6">
 
-        <x-post-featured-card />
+        <x-post-featured-card :post="$posts[0]" />
 
-        <div class="lg:grid lg:grid-cols-2">
+        <div class=" lg:grid lg:grid-cols-2">
             <x-post-card />
             <x-post-card />
         </div>
