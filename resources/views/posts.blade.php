2f3d0abf94c0a25776cdcd13ea299137b4cf8c38
Alexander Hoinville <ahoin001@gmail.com>
2022-06-10 13:36:27 -0400

    Dynamically render featured post and 2 column post grid.

--- resources/views/posts.blade.php
+++ resources/views/posts.blade.php
@@ -6,16 +6,16 @@
 
         <x-post-featured-card :post="$posts[0]" />
 
-        <div class=" lg:grid lg:grid-cols-2">
-            <x-post-card />
-            <x-post-card />
+        <div class="lg:grid lg:grid-cols-2">
+            @foreach ($posts->skip(1) as $post)
+                <x-post-card :post="$post" />
+            @endforeach
         </div>
 
-        <div class="lg:grid lg:grid-cols-3">
-            <x-post-card />
-            <x-post-card />
-            <x-post-card />
-        </div>
     </main>
-
+    {{-- <div class="lg:grid lg:grid-cols-3">
+        <x-post-card />
+        <x-post-card />
+        <x-post-card />
+    </div> --}}
 </x-layout>
